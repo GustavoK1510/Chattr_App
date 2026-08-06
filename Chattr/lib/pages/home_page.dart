0b06f300a9437ca20cce4f7de1cd2c6c47bc14bd
GustavoK1510 <gustavoklc1510@gmail.com>
2026-08-06 10:55:55 -0300

    wip: implement reusable widgets

--- Chattr/lib/pages/home_page.dart
+++ Chattr/lib/pages/home_page.dart
@@ -1,16 +1,44 @@
 import 'package:chattr/components/my_drawer.dart';
+import 'package:chattr/services/auth/auth_service.dart';
+import 'package:chattr/services/chat/chat_service.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  HomePage({super.key});
 
-  const HomePage({super.key});
+  final ChatService _chatService = ChatService();
+  final AuthService _authService = AuthService();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Home"),
       ),
-      drawer: MyDrawer(),
+      drawer: const MyDrawer(),
+      body: _buildUserList(),
     );
+  }
+
+  Widget _buildUserList() {
+    return StreamBuilder(
+      stream: _chatService.getUserStream(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return const Text("Error");
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text("Loading ...");
+        }
+
+        return ListView(
+          children: snapshot.data!.map<Widget>((userData) => _buildUserListItem).toList(), //finish the widget
+        );
+      },
+    );
+  }
+
+  Widget _buildUserListItem(Map<String, dynamic> userData, BuildContext context) {
+    return UserTile(); //add UserTile
   }
 }
