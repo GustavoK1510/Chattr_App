4412548c54a2f448ec47a3901992906c601d4063
GustavoK1510 <gustavoklc1510@gmail.com>
2026-08-14 14:19:55 -0300

    fix: theme set using shared preferences

--- Chattr/lib/themes/theme_provider.dart
+++ Chattr/lib/themes/theme_provider.dart
@@ -1,25 +1,46 @@
 import 'package:chattr/themes/dark_mode.dart';
 import 'package:chattr/themes/light_mode.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class ThemeProvider extends ChangeNotifier{
+class ThemeProvider extends ChangeNotifier {
   ThemeData _themeData = lightMode;
 
   ThemeData get themeData => _themeData;
 
   bool get isDarkMode => _themeData == darkMode;
 
+  ThemeProvider() {
+    loadTheme();
+  }
+
+
   set themeData(ThemeData themeData) {
     _themeData = themeData;
     notifyListeners();
   }
 
-  void toggleTheme() {
+  void toggleTheme() async {
     if (_themeData == lightMode) {
       themeData = darkMode;
     } else {
       themeData = lightMode;
     }
+
+    await saveTheme();
+  }
+
+  Future<void> saveTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool("isDarkMode", isDarkMode);
+  }
+
+  Future<void> loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    
+    final isDark = prefs.getBool("isDarkMode") ?? false;
+
+    themeData = isDark ? darkMode : lightMode;
   }
 
 }
